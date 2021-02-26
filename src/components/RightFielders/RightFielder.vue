d7be023460624c9922b5c5a8603a3e806ac26327
StanimalTheMan <sjc591@nyu.edu>
2021-02-26 16:54:20 -0500

    achanged mouse hover icon, can improve DRY

--- src/components/RightFielders/RightFielder.vue
+++ src/components/RightFielders/RightFielder.vue
@@ -13,4 +13,8 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+img:hover {
+  cursor: pointer;
+}
+</style>
--- src/components/ThirdBasemen/ThirdBaseman.vue
+++ src/components/ThirdBasemen/ThirdBaseman.vue
@@ -12,3 +12,9 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+img:hover {
+  cursor: pointer;
+}
+</style>
